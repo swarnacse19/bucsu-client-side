28189df945a8966878900405fba6a1c14714deed
swarnacse19 <sharna.cse9.bu@gmail.com>
2026-01-25 02:33:15 +0600

    create firebase config file

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register";
 
 export const router = createBrowserRouter([
   { 
     path: "/login", 
     Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
   {
     path: "/",
